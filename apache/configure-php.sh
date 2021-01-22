f3f1d0fab40740dbd78d24fc87bc23d031ef0c52
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2021-01-22 11:34:28 -0300

    configuracion de xdebug funcional y más prolija

--- apache/configure-php.sh
+++ apache/configure-php.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "    => Adding xdebug config"
-sed s/\;\ Local/"[xdebug]\nxdebug.remote_enable = 1\nxdebug.remote_port = 9000\nxdebug.remote_connect_back =1\n \n; Local"/ /etc/php5/apache2/php.ini > /etc/php5/apache2/php.ini.xdebug
 
-mv -f /etc/php5/apache2/php.ini.xdebug  /etc/php5/apache2/php.ini
+echo "[xdebug]" >> /etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_enable = 1" >> /etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back =1" >> /etc/php5/mods-available/xdebug.ini
+
+#otra opción sería ir a php.ini
+#sed s/\;\ Local/"[xdebug]\nxdebug.remote_enable = 1\nxdebug.remote_port = 9000\nxdebug.remote_connect_back =1\n \n; Local"/ /etc/php5/apache2/php.ini > /etc/php5/apache2/php.ini.xdebug
+#mv -f /etc/php5/apache2/php.ini.xdebug  /etc/php5/apache2/php.ini
