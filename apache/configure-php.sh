f8c0ca61a805a76174b0de00fb37d30c5d6fd82e
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2021-01-22 11:37:21 -0300

    Emprolijada de mods dir

--- apache/configure-php.sh
+++ apache/configure-php.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "    => Adding xdebug config"
 
-echo "[xdebug]" >> /etc/php5/mods-available/xdebug.ini
-echo "xdebug.remote_enable = 1" >> /etc/php5/mods-available/xdebug.ini
-echo "xdebug.remote_port = 9000" >> /etc/php5/mods-available/xdebug.ini
-echo "xdebug.remote_connect_back =1" >> /etc/php5/mods-available/xdebug.ini
+PHP_MODS_DIR=/etc/php7.2/mods-available
+
+echo "[xdebug]" >> xdebug.ini
+echo "xdebug.remote_enable = 1" >> ${PHP_MODS_DIR}/xdebug.ini
+echo "xdebug.remote_port = 9000" >> ${PHP_MODS_DIR}/xdebug.ini
+echo "xdebug.remote_connect_back =1" >> ${PHP_MODS_DIR}/xdebug.ini
 
 #otra opción sería ir a php.ini
 #sed s/\;\ Local/"[xdebug]\nxdebug.remote_enable = 1\nxdebug.remote_port = 9000\nxdebug.remote_connect_back =1\n \n; Local"/ /etc/php5/apache2/php.ini > /etc/php5/apache2/php.ini.xdebug
